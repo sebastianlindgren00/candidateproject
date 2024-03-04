c78704d8b37cac7e647ed5223c093f1d1b8b330a
PhilipRobertsson <phiro138@student.liu.se>
2024-03-04 11:50:21 +0100

    Placeholder elements and structure App.js and App.css

--- website/src/App.js
+++ website/src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <h1>SPACE DOME</h1>
+        <label for="gameID">Game ID:</label>
+        <input type="text" id="gameID" name="gID" placeholder="Game ID: 1234"/>
+        <input type="submit" value="Submit"></input>
+      </header>
     </div>
   );
 }
