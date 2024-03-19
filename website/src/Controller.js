0168c5bc6015d30b1927d6c347290b383ad01387
PhilipRobertsson <phiro138@student.liu.se>
2024-03-19 12:40:24 +0100

    Backgrounds added, unnneccesary images removed

--- website/src/Controller.js
+++ website/src/Controller.js
@@ -4,6 +4,11 @@ function Controller() {
     //TODO: Add function to check screen orientation, alt. force screen into landscape
     //TODO: Add controll buttons
     //TODO: Send user info from App.js to this page
+    return(
+        <div className="Controller">
+
+        </div>
+    );
 }
 
 export default Controller;
