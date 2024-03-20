97f9029dcf957fe87a570032af2250c069246065
PhilipRobertsson <phiro138@student.liu.se>
2024-03-20 16:54:35 +0100

    Implemented generator which generates a random UUID for each user. This UUID plus the user name is now sent from the input page to the controller.

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './App.css';
-import { useNavigate } from "react-router-dom";
+import { createSearchParams,useNavigate } from "react-router-dom";
+import {useState} from 'react';
+import {useEffect} from 'react';
 
 
 function App() {
+
+  const [userID, setUserId] = useState('initail')
+  const [userName, setUserName] = useState('inital')
 
   function showInfoBox(){
     const iB = document.getElementById('InfoBox');
@@ -15,20 +20,53 @@ function App() {
     }
   }
 
+  // Source for generateUUID(): https://stackoverflow.com/a/8809472 , acquired 2024-03-20
+  function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
   const navigate = useNavigate();
  
-    const controller = () => {
-        navigate("/controller")
+  useEffect(() => { // Is called every render, thus an if statement is used before the user is navigated to the controller page
+    console.log('State updated: \n', userID,' : ', userName);
+
+    if(userID != 'inital' && userName != 'inital' && userName !=''){
+      navigate({
+          pathname: '/controller',
+          search: createSearchParams({
+            userID:userID,
+            userName:userName
+          }).toString()
+      })
     }
+  }, [userID, userName]);
+
+  const updateUserValues = () => {
+      setUserId(generateUUID())
+      setUserName(document.getElementById("userName").value)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>SPACE DOME</h1>
-        <label htmlFor="gameID">Game ID:</label>
-        <input type="text" id="gameID" name="gID" placeholder="1234"/>
-        <input type="submit" value="Submit" onClick={controller}></input><br></br>
+        <label htmlFor="userName">Namn:</label>
+        <input type="text" id="userName" name="uName" placeholder="Anders Andersson"/>
+        <input type="submit" value="Gå med" onClick={updateUserValues}></input><br></br>
       </header>
-
       <div className="InfoBox" id="InfoBox">
         <button id="InfoButton" type="button" onClick={showInfoBox}>
           INFO
--- website/src/Controller.js
+++ website/src/Controller.js
@@ -1,12 +1,16 @@
 import './Controller.css';
+import './App'
+import { useSearchParams } from 'react-router-dom';
 
 function Controller() {
     //TODO: Add function to check screen orientation, alt. force screen into landscape
     //TODO: Add controll buttons
-    //TODO: Send user info from App.js to this page
+    const [searchparams] = useSearchParams()
+
     return(
         <div className="Controller">
-
+            <p style={{color:'white'}}>UserID: {searchparams.get('userID')}</p>
+            <p style={{color:'white'}}>UserName: {searchparams.get('userName')}</p>
         </div>
     );
 }
