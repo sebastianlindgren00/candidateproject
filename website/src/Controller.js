7b8c3b423786ea54b90e350a6793d148451d3489
PhilipRobertsson <phiro138@student.liu.se>
2024-03-21 14:56:44 +0100

    Screen orientation check fixed

--- website/src/Controller.js
+++ website/src/Controller.js
@@ -1,19 +1,41 @@
 import './Controller.css';
 import './App'
+import {useState, useEffect} from 'react'
 import { useSearchParams } from 'react-router-dom';
 
 function Controller() {
-    //TODO: Add function to check screen orientation, alt. force screen into landscape
     //TODO: Add controll buttons
-
     const [searchparams] = useSearchParams() // Used to search in the URL
+    const [scrOrientation, setOrientation] = useState(window.screen.orientation.type)
 
-    return(
-        <div className="Controller">
-            <p style={{color:'white'}}>UserID: {searchparams.get('userID')}</p>
-            <p style={{color:'white'}}>UserName: {searchparams.get('userName')}</p>
-        </div>
-    );
+    window.screen.orientation.addEventListener('change', function() { setOrientation(window.screen.orientation.type) })
+    //window.screen.orientation.onchange = function(e) { ... }
+    if(window.screen.orientation.type != 'landscape-primary' || window.screen.orientation.type != 'landscape-secondary'){
+        return(
+            <div className="Controller">
+                <label htmlFor="devBox" style={{color:'white'}}> Developer Box
+                    <div className="devBox">
+                        <p style={{color:'white'}}>UserID: {searchparams.get('userID')}</p>
+                        <p style={{color:'white'}}>UserName: {searchparams.get('userName')}</p>
+                        <p style={{color:'white'}}>Screen orientation: {scrOrientation}</p>
+                    </div>
+                </label>
+                <h1 id='orientationDisclaimer' style={{color:'white'}}>Rotera mobilen för att spela</h1>
+            </div>
+        );
+    }else{
+        return(
+            <div className="Controller">
+                <label htmlFor="devBox" style={{color:'white'}}> Developer Box
+                    <div className="devBox">
+                        <p style={{color:'white'}}>UserID: {searchparams.get('userID')}</p>
+                        <p style={{color:'white'}}>UserName: {searchparams.get('userName')}</p>
+                        <p style={{color:'white'}}>Screen orientation: {scrOrientation}</p>
+                    </div>
+                </label>
+            </div>
+        );
+    }
 }
 
 export default Controller;
