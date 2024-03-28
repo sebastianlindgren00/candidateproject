0458d6e56e402848e9d9ad0fbcd2400cd302b50f
Simon Jonsson <simonjonsson2002@gmail.com>
2024-03-28 12:29:13 +0100

    Mer i testfil

---
+++ SpaceDome/src/tests.cpp
@@ -0,0 +1,13 @@
+//lägg till library catch2
+
+//#include <catch2/catch_all.hpp>
+
+//lägg till tester som ska klaras innan programmet mergas på formatet
+
+/*
+TEST_CASE("Connectrion", "[Spacedome]") {
+
+    REQUIRE(ConnectionOmni == true);
+   
+}
+*/
